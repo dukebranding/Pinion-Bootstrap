c85bb43279a2f23d1dad78bc2a515c97cb99d29c
isral Duke <duke@dukebranding.com>
2014-08-12 11:18:45 -0500

    Added Elements:
    Sticky Footer, Nav Bar, Settings Markup

--- js/angular-pieces.js
+++ js/angular-pieces.js
@@ -1,19 +1,48 @@
 (function(){
 	var app = angular.module("pinionApp", []);
 	
+	app.directive('pinionHeader', function(){
+		 return {
+			 restrict: 'E',
+			 templateUrl: '/partials/pinion-header.html'
+		 };
+	});
+	
 	app.directive('pinionFooter', function(){
 		 return {
 			 restrict: 'E',
-<<<<<<< HEAD
 			 templateUrl: '/partials/pinion-footer.html'
 		 };
 	});
 	
-	
-	
-=======
-			 templateUrl: '/partials/footer.html'
+	app.directive('pinionFilters', function(){
+		 return {
+			 restrict: 'E',
+			 templateUrl: '/partials/pinion-filters.html'
 		 };
 	});
->>>>>>> FETCH_HEAD
+	
+	app.directive('pinionPageProgress', function(){
+		 return {
+			 restrict: 'E',
+			 templateUrl: '/partials/pinion-progress.html'
+		 };
+	});
+	
+	app.directive('pinionLoadmore', function(){
+		 return {
+			 restrict: 'E',
+			 templateUrl: '/partials/pinion-loadmore.html'
+		 };
+	});
+	
+	app.directive('pinionSettings', function(){
+		 return {
+			 restrict: 'E',
+			 templateUrl: '/partials/pinion-settings.html'
+		 };
+	});
+	
+	
+	
 })();
