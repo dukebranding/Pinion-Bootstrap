58d3de374f5d6a0f66c3850dc4465b0263016910
isral <duke@dukebranding.com>
2014-09-05 12:11:58 -0500

    Created Flippy List Replacement prototype from BS system

--- js/js.js
+++ js/js.js
@@ -2,8 +2,8 @@ var paragraph1 = "Born: Sapphi, Bo, Biff, Skitch, Crash, Archimedes";
 var paragraph2 = "Died: Lolli, Aristotle, Bunkie";
 
 function catNames(paragraph) {
-	var delimiterChar = paragraph.indexOf(":");
+	var delimiterChar = paragraph.indexOf(": ");
 	var nameString = paragraph.substring(delimiterChar, paragraph.length);
-	var catArray = nameString.split(",");
+	var catArray = nameString.split(", ");
 	return (catArray);
 }
